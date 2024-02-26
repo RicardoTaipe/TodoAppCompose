55aa0bcfcc792eddca568bd7caee7d179714a963
RicardoTaipe <rdavid28@hotmail.es>
2024-02-25 22:23:17 -0500

    add service locator pattern

--- app/src/main/java/com/example/todoappcompose/TodoApplication.kt
+++ app/src/main/java/com/example/todoappcompose/TodoApplication.kt
@@ -1,10 +1,15 @@
 package com.example.todoappcompose
 
 import android.app.Application
+import com.example.todoappcompose.data.source.TasksRepository
 import timber.log.Timber
 import timber.log.Timber.*
 
 class TodoApplication : Application() {
+
+    val taskRepository: TasksRepository
+        get() = ServiceLocator.provideTasksRepository(this)
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) Timber.plant(DebugTree())
