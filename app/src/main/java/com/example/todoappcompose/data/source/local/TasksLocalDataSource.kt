a45884734dbfe01e30dee4c0d47235e88aeaf424
RicardoTaipe <rdavid28@hotmail.es>
2024-02-25 21:43:29 -0500

    add data layer

---
+++ app/src/main/java/com/example/todoappcompose/data/source/local/TasksLocalDataSource.kt
@@ -0,0 +1,92 @@
+package com.example.todoappcompose.data.source.local
+
+import com.example.todoappcompose.data.Result
+import com.example.todoappcompose.data.Result.Success
+import com.example.todoappcompose.data.Result.Error
+import com.example.todoappcompose.data.Task
+import com.example.todoappcompose.data.source.TasksDataSource
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+
+
+class TasksLocalDataSource internal constructor(
+    private val tasksDao: TasksDao,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : TasksDataSource {
+
+    override fun getTasksStream(): Flow<Result<List<Task>>> {
+        return tasksDao.observeTasks().map {
+            Success(it)
+        }
+    }
+
+    override fun getTaskStream(taskId: String): Flow<Result<Task>> {
+        return tasksDao.observeTaskById(taskId).map {
+            Success(it)
+        }
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        // NO-OP
+    }
+
+    override suspend fun refreshTasks() {
+        // NO-OP
+    }
+
+    override suspend fun getTasks(): Result<List<Task>> = withContext(ioDispatcher) {
+        return@withContext try {
+            Success(tasksDao.getTasks())
+        } catch (e: Exception) {
+            Error(e)
+        }
+    }
+
+    override suspend fun getTask(taskId: String): Result<Task> = withContext(ioDispatcher) {
+        try {
+            val task = tasksDao.getTaskById(taskId)
+            if (task != null) {
+                return@withContext Success(task)
+            } else {
+                return@withContext Error(Exception("Task not found!"))
+            }
+        } catch (e: Exception) {
+            return@withContext Error(e)
+        }
+    }
+
+    override suspend fun saveTask(task: Task) = withContext(ioDispatcher) {
+        tasksDao.insertTask(task)
+    }
+
+    override suspend fun completeTask(task: Task) = withContext(ioDispatcher) {
+        tasksDao.updateCompleted(task.id, true)
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        tasksDao.updateCompleted(taskId, true)
+    }
+
+    override suspend fun activateTask(task: Task) = withContext(ioDispatcher) {
+        tasksDao.updateCompleted(task.id, false)
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        tasksDao.updateCompleted(taskId, false)
+    }
+
+    override suspend fun clearCompletedTasks() = withContext<Unit>(ioDispatcher) {
+        tasksDao.deleteCompletedTasks()
+    }
+
+    override suspend fun deleteAllTasks() = withContext(ioDispatcher) {
+        tasksDao.deleteTasks()
+    }
+
+    override suspend fun deleteTask(taskId: String) = withContext<Unit>(ioDispatcher) {
+        tasksDao.deleteTaskById(taskId)
+    }
+}
